ad6bc3cbeda4a7b5bebac4df455bb35fa6707c2f
Gayathri2102 <32144839+gayathri2102@users.noreply.github.com>
2017-11-02 00:03:16 +0530

    The SMS application was created. But still error persists. The errors in logic are to be rectified. The method in which SMS to be sent on clicking the button is to be figured out.

--- app/src/main/java/com/example/gayathribala/sms/MainActivity.java
+++ app/src/main/java/com/example/gayathribala/sms/MainActivity.java
@@ -1,7 +1,11 @@
+
 package com.example.gayathribala.sms;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+        Button btnSendSMS = (Button) findViewById(R.id.button_sms);
+        btnSendSMS.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                            }
+        });}
+
