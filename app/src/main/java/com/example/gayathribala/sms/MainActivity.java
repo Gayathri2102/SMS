3fa59f468ff8d2c8a48018e650d2fd121832ab55
Gayathri2102 <32144839+gayathri2102@users.noreply.github.com>
2017-11-19 01:21:46 +0530

    Code for calling the registered mobile number is added. Further testing is done.

--- app/src/main/java/com/example/gayathribala/sms/MainActivity.java
+++ app/src/main/java/com/example/gayathribala/sms/MainActivity.java
@@ -1,6 +1,9 @@
 
 package com.example.gayathribala.sms;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.SmsManager;
@@ -15,10 +18,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Button btnSendSMS = (Button) findViewById(R.id.button_sms);
         btnSendSMS.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("MissingPermission")
             public void onClick(View v) {
                 sendSMS("9029894202", "Guess what !! I am Safe!");
            /*here i can send message to emulator 5556. In Real device
             *you can change number*/
+
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:0377778888"));
+                startActivity(callIntent);
+
             }
         });}
 
+    private void sendSMS(String phoneNumber, String message) {
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage(phoneNumber, null, message, null, null);
+    }}
